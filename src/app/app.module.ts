4842ea8af166f71d3945a8be19a92ad56d742c75
DimitriRO-IBM <dimitri.rodrigues-ISC_France@fr.ibm.com>
2019-12-05 16:04:18 +0100

    Image offline storage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  NbThemeModule,
+  NbLayoutModule,
+  NbInputModule,
+  NbButtonModule,
+  NbAccordionModule, NbSpinnerModule
+} from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'cosmic' }),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    NbInputModule,
+    NbButtonModule,
+    NbAccordionModule,
+    AppRoutingModule,
+    FormsModule,
+    NbSpinnerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
